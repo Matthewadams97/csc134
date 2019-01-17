ce89b107ea9992fe1e4eb957a44be8077e20ad13
Matthewadams97 <46724938+Matthewadams97@users.noreply.github.com>
2019-01-17 14:00:40 -0500

    Add files via upload

---
+++ M1HW_adams/main.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M1HW1
+// M. ADams
+// 1/17/19
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // introducing myself through code
+    cout << "Hello, My name is Matthew Adams" << endl;
+    cout << "My current program is engineering" << endl;
+    cout << "I started programming in" << 2018 << endl;
+    cout << "I have "  << 1 << " pet" << endl;
+    cout << "\ta dog" << endl;
+    cout << "I dont have any favorite T.v shows but I enjoy playing video games on my pc." << endl;
+    return 0;
+}
